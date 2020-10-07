89feb51b2ef9e608b4e134fb01cf955c400846ed
Corentin Cadiou <contact@cphyc.me>
2020-10-07 12:16:20 +0100

    Move src files in their own folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './change_message.js',
+  entry: './src/change_message.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'change_message.js'
